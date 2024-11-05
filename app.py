204db5c58bd8dc816622fc4723775fed7872152c
Leonora Goble <leonora.goble@colorado.edu>
2024-11-05 15:13:07 -0600

    db insert

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from Leonora Goble in 3308!'
 
-@app.route('db_test')
+@app.route('/db_test')
 def db_test():
     conn = psycopg2.connect('postgresql://leonorae_postgres_hello_user:ekwIlLb9DFxBgjvrnFH7IcgKvqHBYzGU@dpg-csl8eem8ii6s73c1e47g-a/leonorae_postgres_hello')
     conn.close()
     return 'Database Connection Successful'
+
